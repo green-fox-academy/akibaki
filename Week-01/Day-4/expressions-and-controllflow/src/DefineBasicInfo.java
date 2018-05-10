51a18d406079f4f0a739e76a1c9802d355b11d98
Akibaki <beinak@gmail.com>
2018-05-10 17:06:05 +0200

    DefineBasicInfo

---
+++ Week-01/Day-4/expressions-and-controllflow/src/DefineBasicInfo.java
@@ -0,0 +1,15 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+
+        String name ="Akos";
+        // Your age as an integer
+        int age=20;
+        // Your height in meters as a double
+        double height = 1.85;
+        boolean state=false;
+        // Whether you are married or not as a boolean
+        System.out.println(name+ " Age: "+String.valueOf(age)+" Height: "+String.valueOf(height)+" Married: "+String.valueOf(state));
+    }
+}
