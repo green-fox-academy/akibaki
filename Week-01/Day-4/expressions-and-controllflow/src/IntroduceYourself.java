f841225797c35d23ae2723a50b65920cd2100563
Akibaki <beinak@gmail.com>
2018-05-10 14:08:42 +0200

    IntroduceYourself

---
+++ Week-01/Day-4/expressions-and-controllflow/src/IntroduceYourself.java
@@ -0,0 +1,17 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        // Write a program that prints a few details to the terminal window about you
+        // It should print each detail to a new line.
+        //  - Your name
+        //  - Your age
+        //  - Your height in meters (as a decimal fraction)
+        //
+        //  Example output:
+        //  John Doe
+        //  31
+        //  1.87
+        System.out.println("My name is Ákos!");
+        System.out.println("I am 20!");
+        System.out.println("I am 1.85m high.");
+    }
+}
