6bb57e9cb1f5372d1956b336511d9b08224a8af0
Akibaki <beinak@gmail.com>
2018-05-10 17:52:09 +0200

    SecondsInADay

---
+++ Week-01/Day-4/expressions-and-controllflow/src/SecondInADay.java
@@ -0,0 +1,11 @@
+public class SecondInADay {
+    public static void main(String[] args) {
+        int currentHours = 14;
+        int currentMinutes = 34;
+        int currentSeconds = 42;
+
+        // Write a program that prints the remaining seconds (as an integer) from a
+        // day if the current time is represented by the variables
+        System.out.println(86400-currentHours*3600-currentMinutes*60-currentSeconds);
+    }
+}
