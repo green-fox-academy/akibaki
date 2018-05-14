cebf63da8010d19f98d425c00df776165cd89f73
Akibaki <beinak@gmail.com>
2018-05-14 15:25:00 +0200

    Colors

--- Week-02/Day-01/src/Matrix.java
+++ Week-02/Day-01/src/Matrix.java
@@ -3,12 +3,13 @@ import java.util.Arrays;
 public class Matrix {
   public static void main(String[] args) {
     // - Create (dynamically) a two dimensional array
+    int size=6;
 
-    int[][] matrix=new int[4][4];
+    int[][] matrix=new int[size][size];
     int number=0;
 
-    for (int i = 0; i < 4; i++) {
-      for (int j = 0; j <4; j++) {
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j <size; j++) {
         if (j==i){number=1;}
         else {number=0;};
         matrix[i][j]=number;
