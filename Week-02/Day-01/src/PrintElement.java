83b6905fb00012cbe06bfca3c034b42ac11a7e86
Akibaki <beinak@gmail.com>
2018-05-14 14:44:08 +0200

    PrintElement

---
+++ Week-02/Day-01/src/PrintElement.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class PrintElement {
+  public static void main(String[] args) {
+    // - Create an array variable named `numbers`
+//   with the following content: `[4, 5, 6, 7]`
+    int[] numbers={4,5,6,7};
+    System.out.println(Arrays.toString(numbers));
+// - Print all the elements of `numbers`
+  }
+}
