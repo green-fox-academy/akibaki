5594ac11235e53a881ae2e4b2f9ff44144923fc6
Akibaki <beinak@gmail.com>
2018-05-14 14:41:51 +0200

    IncreaseElement

---
+++ Week-02/Day-01/src/IncreaseElement.java
@@ -0,0 +1,12 @@
+public class IncreaseElement {
+  public static void main(String[] args) {
+    //- Create an array variable named `t`
+    //with the following content: `[1, 2, 3, 4, 5]`
+
+    int[] t={1,2,3,4,5};
+    //- Increment the third element
+    t[2]+=1;
+    System.out.println(t[2]);
+    //        - Print the third element
+  }
+}
