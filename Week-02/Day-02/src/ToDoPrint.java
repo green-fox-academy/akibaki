f8f77ee57a6197313f77f918b2a8309d5634a987
Akibaki <beinak@gmail.com>
2018-05-15 12:54:25 +0200

    ToDoPrint

---
+++ Week-02/Day-02/src/ToDoPrint.java
@@ -0,0 +1,21 @@
+public class ToDoPrint {
+  public static void main(String... args){
+    String todoText = " - Buy milk\n";
+
+    // Add "My todo:" to the beginning of the todoText
+    todoText="My todo:\n"+todoText;
+    // Add " - Download games" to the end of the todoText
+    todoText+=" - Download games\n";
+    // Add " - Diablo" to the end of the todoText but with indention
+    todoText+="  - Diablo";
+
+    // Expected outpt:
+
+    // My todo:
+    //  - Buy milk
+    //  - Download games
+    //      - Diablo
+
+    System.out.println(todoText);
+  }
+}
