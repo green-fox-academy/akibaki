41b7a6f9c621d9c20c3a30d2a60163c6f1e5a362
Akibaki <beinak@gmail.com>
2018-05-16 15:05:55 +0200

    PrintEachLine

--- Week-02/Day-03/File-Manipulations/src/PrintEachLine.java
+++ Week-02/Day-03/File-Manipulations/src/PrintEachLine.java
@@ -1,15 +1,34 @@
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
-  public class PrintEachLine {
-    public static void main(String[] args) {
-      // Write a program that opens a file called "my-file.txt", then prints
-      // each of lines form the file.
-      // If the program is unable to read the file (for example it does not exists),
-      // then it should print an error message like: "Unable to read file: my-file.txt"
+public class PrintEachLine {
+  public static void main(String[] args) {
+//C:\Users\Dell\Desktop\greenfox\akibaki\Week-02\Day-03\File-Manipulations\
+    String fileName = "cica.txt";
+    Path path = Paths.get(fileName);
+    System.out.println(path);
 
-
+    try {
+      List<String> content = Files.readAllLines(path);
+      for (String rows : content) {
+        System.out.println(rows);
+      }
+    } catch (IOException e) {
+      System.out.println("file doesnt exist");
     }
+
+
+    // Write a program that opens a file called "my-file.txt", then prints
+    // each of lines form the file.
+    // If the program is unable to read the file (for example it does not exists),
+    // then it should print an error message like: "Unable to read file: my-file.txt"
+
+
   }
+}
+
