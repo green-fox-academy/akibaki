898e356c8b6f7dd65500956ccb5a1401e5aa5453
Akibaki <beinak@gmail.com>
2018-05-17 17:18:32 +0200

    RainBowBoxes

--- Week-02/Day-04/src/RainBowBoxes.java
+++ Week-02/Day-04/src/RainBowBoxes.java
@@ -3,50 +3,38 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class CenteredBoxFunction {
+public class RainBowBoxes {
 
-    // create a square drawing function that takes 1 parameter:
-    // the square size
-    // and draws a square of that size to the center of the canvas.
-    // draw 3 squares with that function.
-    // avoid code duplication.
+    // Create a square drawing function that takes 2 parameters:
+    // The square size, and the fill color,
+    // and draws a square of that size and color to the center of the canvas.
+    // Create a loop that fills the canvas with rainbow colored squares.
 
 
   public static void mainDraw(Graphics graphics) {
-    int[]as={25,154,234};
-    for (int a : as) {
-      Rectangle(graphics, a);
+    for (int i = 200; i >0; i-=10) {
+      Rectangle(graphics, i, color(graphics));
     }
   }
 
-  public static void Rectangle(Graphics graphics, int a) {
+  public static void Rectangle(Graphics graphics, int a, Color color) {
+
+    graphics.setColor(color);
     int b=a;
     int x0=WIDTH/2-a/2;
     int y0=(HEIGHT-23)/2-b/2;
 
-    int x1 = x0;
-    int y1 = y0;
-    int x2 = 0;
-    int y2 = 0;
-
-
-    for (int i = 0; i <4; i++) {
-      x2=x1+(int)(Math.sin(Math.PI*i/2)*a);
-      y2=y1+(int)(Math.cos(Math.PI*i/2)*b);
-      cc(graphics);
-      graphics.drawLine(x1, y1, x2, y2);
-      x1=x2;
-      y1=y2;
+    graphics.fillRect(x0, y0 , a, b);
     }
-  }
-  public static void cc(Graphics graphics) {
+
+  public static Color color(Graphics graphics) {
     int r = (int) (Math.random() * 256);
     int g = (int) (Math.random() * 256);
     int b = (int) (Math.random() * 256);
-    graphics.setColor(new Color(r, g, b));
+    return new Color(r,g,b);
   }
 
-  //    Don't touch the code below
+  // Don't touch the code below
   static int WIDTH = 320;
   static int HEIGHT = 343;
 
@@ -58,12 +46,12 @@ public class CenteredBoxFunction {
     jFrame.setLocationRelativeTo(null);
     jFrame.setVisible(true);
   }
-  static class ImagePanel extends JPanel{
+
+  static class ImagePanel extends JPanel {
     @Override
     protected void paintComponent(Graphics graphics) {
       super.paintComponent(graphics);
       mainDraw(graphics);
-
     }
   }
 }
