96923e044e94e2bb6f13a0f93ce3c44581e61ed8
Akibaki <beinak@gmail.com>
2018-05-24 11:41:36 +0200

    Counter

---
+++ Week-03/Day-04/Counter/src/Counter.java
@@ -0,0 +1,17 @@
+public class Counter {
+
+  public static int Counter(int n, int it, int sum){
+    if (it <= n) {
+      sum = it + Counter(n, it+1, sum);
+      return sum;
+    }
+    else{
+      return sum;
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println(Counter(50,0, 0));
+  }
+
+}
