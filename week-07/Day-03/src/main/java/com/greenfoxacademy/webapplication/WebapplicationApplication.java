f7fe04a1114688f43ce8dcab511ca65be491ce3e
Akibaki <beinak@gmail.com>
2018-06-27 14:43:13 +0200

    (fature) HelloBeanWorld

---
+++ week-07/Day-03/src/main/java/com/greenfoxacademy/webapplication/WebapplicationApplication.java
@@ -0,0 +1,22 @@
+package com.greenfoxacademy.webapplication;
+
+import com.greenfoxacademy.webapplication.Services.Printer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapplicationApplication implements CommandLineRunner {
+  @Autowired
+  Printer printer;
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebapplicationApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    printer.log("Hello");
+  }
+}
