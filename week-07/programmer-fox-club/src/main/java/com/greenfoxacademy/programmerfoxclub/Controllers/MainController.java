fc81776cfded382549ac3d7a6baefa781f5b3418
Akibaki <beinak@gmail.com>
2018-06-29 14:23:45 +0200

    Nutrition Store

--- week-07/programmer-fox-club/src/main/java/com/greenfoxacademy/programmerfoxclub/Controllers/MainController.java
+++ week-07/programmer-fox-club/src/main/java/com/greenfoxacademy/programmerfoxclub/Controllers/MainController.java
@@ -1,36 +1,34 @@
 package com.greenfoxacademy.programmerfoxclub.Controllers;
 
 import com.greenfoxacademy.programmerfoxclub.Models.Fox;
-import org.springframework.expression.spel.SpelEvaluationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.thymeleaf.exceptions.TemplateInputException;
 
 import java.util.*;
 
 @Controller
 public class MainController {
 
-  private Map<String,Fox> foxes;
+  private Map<String, Fox> foxes;
 
-  MainController (){
+  MainController() {
     foxes = new HashMap<>();
     List<String> tricks = new ArrayList<String>(Arrays.asList("Write in Html", "Code in Java"));
     foxes.put("Mr. Fox", new Fox("Mr. Fox", "pizza", "lemonade", tricks));
   }
 
   @GetMapping("/")
-  public String index(@RequestParam (value = "name", required = false)String name , Model model) {
-    if (name == null||name.equals("")){
+  public String index(@RequestParam(value = "name", required = false) String name, Model model) {
+    if (name == null || name.equals("")) {
       name = "Mr. Fox";
     }
-    if (foxes.get(name) == null){
+    if (foxes.get(name) == null) {
       return "redirect:/login/?name=" + name;
-    }else {
+    } else {
       Fox fox = foxes.get(name);
       model.addAttribute("fox", fox);
       return "index";
@@ -38,20 +36,43 @@ public class MainController {
   }
 
   @GetMapping("/login")
-  public String showLogin(@RequestParam (value = "name", required = false)String name, Model model) {
-    if (name == null){
+  public String showLogin(@RequestParam(value = "name", required = false) String name, Model model) {
+    if (name == null) {
       name = "Name of fox";
-      model.addAttribute( "error", false);
-    } else{
-      model.addAttribute( "error", true);
+      model.addAttribute("error", false);
+    } else {
+      model.addAttribute("error", true);
     }
     model.addAttribute("name", name);
     return "login";
   }
 
   @PostMapping("/login")
-  public String submitLogin(@RequestParam ("name") String name) {
+  public String submitLogin(@RequestParam("name") String name) {
     foxes.put(name, new Fox(name));
+    return "redirect:/?name=" + name;
+  }
+
+
+  @GetMapping("/nutritionStore")
+  public String nutritionStore(@RequestParam(value = "name", required = false) String name, Model model) {
+    if (name == null || name.equals("")) {
+      name = "Mr. Fox";
+    }
+    if (foxes.get(name) == null) {
+      return "redirect:/login/?name=" + name;
+    } else {
+      Fox fox = foxes.get(name);
+      model.addAttribute("fox", fox);
+      return "nutritionStore";
+    }
+  }
+
+  @PostMapping("/nutritionStore/{name}")
+  public String changeNutrition(@RequestParam("food") String food, @RequestParam("drink") String drink, @PathVariable("name")String name) {
+    foxes.get(name).setFood(food);
+    foxes.get(name).setDrink(drink);
+    foxes.put(name, foxes.get(name) );
     return "redirect:/?name=" + name;
   }
 }
